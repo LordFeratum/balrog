2d0bda5054ab5dba806d5312b259c3304d1326b1
Quique Piña <epina@apsl.net>
2018-07-17 09:37:47 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name="balrog",
+    version="0.1",
+    description="A simple RBAC library for Python",
+    author="Enrique Piña Monserrat",
+    packages=["balrog"],
+    provides=["balrog"],
+)
+
