48577fbba218e090ba64b10e51232b775b4708e5
Quique Piña <epina@apsl.net>
2018-07-19 13:42:27 +0200

    modified setup metadata

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ from setuptools import setup
 
 
 setup(
-    name="balrog",
+    name="balrog-rbac",
     version="0.1",
     description="A simple RBAC library for Python",
-    author="Enrique Piña Monserrat",
+    author="Enrique Pina Monserrat",
+    author_email='lord.feratum@gmail.com',
+    url='https://github.com/LordFeratum/balrog',
+    download_url='https://github.com/LordFeratum/balrog/archive/0.1.tar.gz',
     packages=["balrog"],
     provides=["balrog"],
 )
-
